12ce14b75d3f876fa0be50deb0d590960b61d2d5
Rafael Melo Sabino <rafael.sabino@cwi.com.br>
2021-07-15 18:16:18 -0300

    teste

--- src/main/java/br/com/cwi/apus/web/service/SenderMailService.java
+++ src/main/java/br/com/cwi/apus/web/service/SenderMailService.java
@@ -1,2 +1,21 @@
-package br.com.cwi.apus.web.service;public class SenderMailService {
+package br.com.cwi.apus.web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SenderMailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public  void enviar() {
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setTo("joao@teste.com.br");
+        email.setSubject("Pedido 555555");
+        email.setText("Seu pedido 555555 foi Criado com sucesso.");
+        mailSender.send(email);
+    }
 }
